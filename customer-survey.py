4de2722c696e5e03ffc152c235ecdc6396020272
Les Pounder <lespounder@gmail.com>
2018-04-18 15:59:39 +0000

    Updated with working code for the buttons

--- customer-survey.py
+++ customer-survey.py
@@ -1,24 +1,39 @@
 #Quick change for github
-import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab as pl
 import time
-
+from gpiozero import Button
+#Buttons
+yes = Button(17)
+no = Button(27)
+stats = Button(22)
+#Reset Counter
 happy = 0
 unhappy = 0
 
+print("Did you enjoy the event today? Press Green for Yes, Red for No")
 
-happy = int(input("Happy Stats"))
-unhappy = int(input("Unhappy Stats"))
- 
-objects = ('Happy', 'Unhappy')
-y_pos = np.arange(len(objects))
-satisfaction = [happy,unhappy]
- 
-plt.bar(y_pos, satisfaction, align='center', alpha=1)
-plt.xticks(y_pos, objects)
-plt.ylabel('No. of people')
-plt.title('Satisfaction')
-plt.savefig(str(time.ctime())+'.png') 
-plt.show()
+while True:
+    if yes.is_pressed:
+        happy += 1
+        print("Happy",str(happy))
+        time.sleep(1)
+    elif no.is_pressed:
+        unhappy += 1
+        print("Unhappy",str(unhappy))
+        time.sleep(1)
+    elif stats.is_pressed:
+        objects = ('Happy', 'Unhappy')
+        y_pos = np.arange(len(objects))
+        satisfaction = [happy,unhappy]
+        plt.bar(y_pos, satisfaction, align='center', alpha=1)
+        plt.xticks(y_pos, objects)
+        plt.ylabel('No. of people')
+        plt.title('Satisfaction')
+        plt.savefig(str(time.ctime())+'.png') 
+        plt.show()
+        time.sleep(1)
+
 
